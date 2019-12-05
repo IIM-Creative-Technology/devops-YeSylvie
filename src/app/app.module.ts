a355986f705014a149d27732e575c4c0d5ea7a6c
Lee <lycelinepro99@gmail.com>
2019-12-05 14:49:56 +0100

    Maquette V1 - Angular Environnement

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { QuizComponent } from './main/quiz/quiz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/social-wall/social-wall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-wall',
+  templateUrl: './social-wall.component.html',
+  styleUrls: ['./social-wall.component.scss']
+})
+export class SocialWallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
