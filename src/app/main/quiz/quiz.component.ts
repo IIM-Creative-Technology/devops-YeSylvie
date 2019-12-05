4f71ded350b78ac0682584a5e0f5af168cb800aa
Lee <lycelinepro99@gmail.com>
2019-12-05 23:09:19 +0100

    Maquette V1 - Front : add quiz

--- src/app/main/quiz/quiz.component.ts
+++ src/app/main/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +6,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-
-  constructor() { }
+  questions: any;
+  currentIndex: number;
+  notAttempted: any;
+  correct: any;
+  currentQuestionSet: any;
+  start = false;
+  gameover = false;
+  buttonname = 'Play';
 
   ngOnInit() {
   }
 
-}
+  constructor() {
+
+    // JSON DATA QUESTIONS/RESPONSE
+
+    this.questions = [
+      {
+        id: 1,
+        question: 'Quel est ton dessert favori ?',
+        option: [
+          {optionid: 1, name: 'Tiramisu'},
+          {optionid: 2, name: 'Glace'},
+          {optionid: 3, name: ' Perle de coco'},
+          {optionid: 4, name: ' Gateau au chocolat'}
+        ],
+        selected: 0
+      },
+      {
+        id: 2,
+        question: 'Quel est ton sport favori ?',
+        option: [
+          {optionid: 1, name: 'Tiramisu'},
+          {optionid: 2, name: 'Glace'},
+          {optionid: 3, name: ' Perle de coco'},
+          {optionid: 4, name: ' Gateau au chocolat'}
+        ],
+        selected: 0
+      },
+      {
+        id: 3,
+        question: 'Quel est ton acteur favori ?',
+        option: [
+          {optionid: 1, name: 'Tiramisu'},
+          {optionid: 2, name: 'Glace'},
+          {optionid: 3, name: ' Perle de coco'},
+          {optionid: 4, name: ' Gateau au chocolat'}
+        ],
+        selected: 0
+      },
+      {
+        id: 4,
+        question: 'Quel est ton groupe favori ?',
+        option: [
+          {optionid: 1, name: 'Tiramisu'},
+          {optionid: 2, name: 'Glace'},
+          {optionid: 3, name: ' Perle de coco'},
+          {optionid: 4, name: ' Gateau au chocolat'}
+        ],
+        selected: 0
+      }
+    ];
+
+    this.currentIndex = 0;
+    this.currentQuestionSet = this.questions[this.currentIndex];
+  }
+
+   next() {
+     this.currentIndex = this.currentIndex + 1;
+     this.currentQuestionSet = this.questions[this.currentIndex];
+   }
+
+   prev() {
+    this.currentIndex = this.currentIndex + 1;
+    this.currentQuestionSet = this.questions[this.currentIndex];
+  }
+
+   submit() {
+   }
+
+  }
+
+
+
+
