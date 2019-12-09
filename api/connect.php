31ae6a55b849b66f53eaa2e114dd4f88bc61bdbe
YeSylvie <ye.sylvie1803@gmail.com>
2019-12-09 13:09:59 +0000

    send data from request http to php

--- api/connect.php
+++ api/connect.php
@@ -1,6 +1,10 @@
 <?php
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=hommage;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
+        
+        $bdd = new PDO('mysql:host=euphoriaaqmybdd.mysql.db;dbname=euphoriaaqmybdd;charset=utf8', 'euphoriaaqmybdd', 'Studiose2019', array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
+        
+        // En local
+        // $bdd = new PDO('mysql:host=localhost;dbname=hommage;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
     }
 
     catch (PDOException $e) {
